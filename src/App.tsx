1cfbf964e22dd6f8ba4ba4083a39a5f86a5f644a
VVVV BBBB <Volodymyr>
2024-03-21 23:12:12 +0200

    start project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-my-accent'>Test</h1>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
